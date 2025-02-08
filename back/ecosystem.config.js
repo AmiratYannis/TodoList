ba739bfad5da98a1594ac26e24f8df956c09a043
AmiratYannis <46192264+AmiratYannis@users.noreply.github.com>
2025-02-08 12:20:15 +0100

    Change dockerfile and docker-compose

---
+++ back/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+        {
+            name: 'backend',
+            script: 'app.js',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+};
+
+
+
+
